aa1fcbba0ea534acad22e6cdc475991aaeec051d
paragorika <sofiya.krasilnikova@ensaama.net>
2023-11-29 11:10:11 +0100

    etoiles

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const main = document.querySelector('main')
-const arrowSource = document.querySelector('.arrow')
+const main = document.querySelector('.dynamic')
+const starSource = document.querySelector('.four-pointed-star')
 
 main.onclick = (event) => {
     console.log(event.clientY)
 
-    const clone = arrowSource.cloneNode(true)
-    clone.style.top = `${event.clientY}px`
+    const clone = starSource.cloneNode(true)
+    clone.style.left = `${event.clientY}px`
     main.append(clone)
 }
